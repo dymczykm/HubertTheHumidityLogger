2f9acf3058984269bd55209c6deba7baf7abc704
Marcin Dymczyk <marcin.dymczyk@sevensense.ch>
2023-01-04 22:42:28 +0100

    POST and GET work with Adafruit IO.

--- humidity_logger/humidity_logger.ino
+++ humidity_logger/humidity_logger.ino
@@ -14,8 +14,6 @@ String adafruit_io_key = "aio_JeFc31mpZomKU8KaxpU8QTDlmT5n";
 #define LTE_PWRKEY_PIN 5
 #define LTE_FLIGHT_PIN 7
 
-//#define Serial SerialUSB
-
 BME280 bme280;
 
 void setup()
@@ -29,7 +27,7 @@ void setup()
     
     SerialUSB.println("Init");
   
-    pinMode(LTE_RESET_PIN, OUTPUT);                                     //Set up LTE pin modes and initial states
+    pinMode(LTE_RESET_PIN, OUTPUT);
     digitalWrite(LTE_RESET_PIN, LOW);
     pinMode(LTE_PWRKEY_PIN, OUTPUT);
     digitalWrite(LTE_RESET_PIN, LOW);
@@ -38,107 +36,123 @@ void setup()
     delay(2000);
     digitalWrite(LTE_PWRKEY_PIN, LOW);
     pinMode(LTE_FLIGHT_PIN, OUTPUT);
-    digitalWrite(LTE_FLIGHT_PIN, LOW);                                 //Normal Mode
+    digitalWrite(LTE_FLIGHT_PIN, LOW);
+
+    delay(1000);
+    
+    sendData("AT+CRESET\r\n", 500, DEBUG);
+    delay(10000);
+
+    String status_response;
+    int status_code = -1;
+    do {
+      delay(1000);
+      status_response = sendData("AT+CGREG?\r\n", 500, DEBUG);
+      const int comma_idx = status_response.indexOf(",");
+      status_code = status_response.substring(comma_idx + 1, comma_idx + 2).toInt();
+      SerialUSB.println("CGREG status: " + (String)status_code);
+    } while (status_code != 1);
     
     SerialUSB.println("LTE init complete.");
-    
-    delay(5000);                                                        //Wait 5 seconds
 
-//    if(!bme280.init()) {                                                  //Connect to pressure sensor
+//    if(!bme280.init()) {
 //      Serial.println("BME280 init error!");
 //    }
 
     SerialUSB.println("BME init complete.");
+
+    delay(1000);
     
-    delay(10000);                                                        //Wait 1 second for sensors to start
-
     SerialUSB.println("Setup complete.");
+}
 
-    sendData("ATI\r\n", 500, DEBUG);
+bool postToFeed(String feed, float value) {
+  String url_str = "AT+HTTPPARA=\"URL\",\"https://io.adafruit.com/api/v2/mdymczyk/feeds/garaz." + feed + "/data?x-aio-key=" + adafruit_io_key + "\"\r\n";
+  String content_str = "AT+HTTPPARA=\"CONTENT\",\"application/json\"\r\n";
+  
+  // This doesn't work, seems to be a bug within SIM7600. It definitely doesn't show up in the outgoing request.
+  //String aio_key_str = "AT+HTTPPARA=\"USERDATA\",\"x-aio-key: <key>\"\r\n"; 
+  
+  String data_json = "{\"value\": " + (String)value + ", \"lat\": 23.1, \"lon\": \"-73.3\"}";
+  
+  String data_cmd_str = "AT+HTTPDATA=" + String(data_json.length()) + ",9900\r\n";
+  
+  sendData("AT+HTTPINIT\r\n", 2000, DEBUG);
+  
+  sendData(url_str, 1000, DEBUG);
+  sendData(content_str, 1000, DEBUG);
+  
+  sendData(data_cmd_str, 500, DEBUG);
+  delay(100);
+  sendData(data_json, 1000, DEBUG);
+  
+  String response = sendData("AT+HTTPACTION=1\r\n", 10000, DEBUG);
+  if (response.length() == 0) {
+    SerialUSB.println("Error parsing the response.");
+    return false;
+  }
+  const int first_comma_idx = response.indexOf(",");
+  const int second_comma_idx = response.indexOf(",", first_comma_idx + 1);
+  
+  String http_code = response.substring(first_comma_idx + 1, second_comma_idx);
+  SerialUSB.println("Response code: " + http_code);
+  
+  sendData("AT+HTTPTERM\r\n", 500, DEBUG);
 
-    sendData("AT+CGMR\r\n", 500, DEBUG);
+  if (http_code == "200") {
+    return true;
+  } else {
+    SerialUSB.println("Failed: " + response);
+    return false;
+  }
+}
 
-    sendData("AT+HTTPTERM\r\n", 500, DEBUG);
+bool getFeed(String feed) {
+  String url_str = "AT+HTTPPARA=\"URL\",\"https://io.adafruit.com/api/v2/mdymczyk/feeds/garaz." + feed + "/data?x-aio-key=" + adafruit_io_key + "\"\r\n";
+  String content_str = "AT+HTTPPARA=\"CONTENT\",\"application/json\"\r\n";
+  
+  // This doesn't work, seems to be a bug within SIM7600. It definitely doesn't show up in the outgoing request.
+  //String aio_key_str = "AT+HTTPPARA=\"USERDATA\",\"x-aio-key: <key>\"\r\n"; 
+  
+  sendData("AT+HTTPINIT\r\n", 2000, DEBUG);
+  
+  sendData(url_str, 1000, DEBUG);
+  sendData(content_str, 1000, DEBUG);
+  
+  String response = sendData("AT+HTTPACTION=0\r\n", 10000, DEBUG);
 
-    sendData("AT+CGSOCKCONT=1,\"IP\",\"internet\"\r\n", 500, DEBUG);
+  sendData("AT+HTTPREAD=0,100\r\n", 500, DEBUG);
+
+  sendData("AT+HTTPTERM\r\n", 500, DEBUG);
+
+  return true;
 }
 
 void loop()
 {
-    float temperature = 0; //bme280.getTemperature();
-    float pressure_hpa = 0; //bme280.getPressure() / 100;
-    float humidity = 0; //bme280.getHumidity(); 
-    temperature = 21;
-    pressure_hpa = 1000;
-    humidity = 55;
-    SerialUSB.print("Temp: ");
-    SerialUSB.print(temperature);
-    SerialUSB.println("C");
-    SerialUSB.print("Humidity: ");
-    SerialUSB.print(humidity);
-    SerialUSB.println("%");
-    SerialUSB.print("Pressure: ");
-    SerialUSB.print(pressure_hpa);
-    SerialUSB.println("hPa");
+  float temperature = 0; //bme280.getTemperature();
+  float pressure_hpa = 0; //bme280.getPressure() / 100;
+  float humidity = 0; //bme280.getHumidity(); 
+  temperature = 21;
+  pressure_hpa = 1000;
+  humidity = 55;
+  SerialUSB.print("Temp: ");
+  SerialUSB.print(temperature);
+  SerialUSB.println("C");
+  SerialUSB.print("Humidity: ");
+  SerialUSB.print(humidity);
+  SerialUSB.println("%");
+  SerialUSB.print("Pressure: ");
+  SerialUSB.print(pressure_hpa);
+  SerialUSB.println("hPa");
+  
+  postToFeed("temperature", temperature);
+  postToFeed("pressure", pressure_hpa);
+  postToFeed("humidity", humidity);
 
-    //String http_str = "AT+HTTPPARA=\"URL\",\"https://api.thingspeak.com/update?api_key=" + Apikey + "&field1=" + (String)temperature + "&field2=" + (String)humidity + "&field3=" + (String)pressure_hpa + "\"\r\n";
-    //SerialUSB.println(http_str);                                     //Print to serial monitor for debugging
+  getFeed("pump");
 
-    //String http_str = "AT+HTTPPARA=\"URL\",\"https://io.adafruit.com/api/v2/mdymczyk/feeds/temperature/data\"\r\n";
-    String http_str = "AT+HTTPPARA=\"URL\",\"https://cvgdg.requestcatcher.com/test\"\r\n";
-    //String http_str2 = "AT+HTTPPARA=\"CONTENT\",\"application/json\r\naaa: bbb\"\r\n";
-    //String http_str3 = "AT+HTTPPARA=\"ACCEPT\",\"*/*\"\r\n";
-    //String http_str35 = "AT+HTTPPARA=\"UA\",\"curl/7.79.1\"\r\n";
-    //String http_str4 = "AT+HTTPPARA=\"USERDATA\",\"x-aio-key: aio_aaaaaaaaaaaaaaa\"\r\n"; //aio_JeFc31mpZomKU8KaxpU8QTDlmT5n\"\r\n";
-    //String http_str41 = "AT+HTTPPARA=\"USERDATA\",\"http_acceptsddss: */*\"\r\n";
-    //String http_str4 = "AT+HTTPPARA=\"USERDATA\",\"Authorization:JWT " + (String)"aaa\\r\\n" + (String)"\"" + (String)"\r\n";
-    String http_str4 = "AT+HTTPPARA=\"USERDATA\",\"Authorization: Basic Y2FycGx1c2dvOmNhcnBsdXgz\"\r\n";
-
-    //String http_str4 = "AT+HTTPPARA=\"USERDATA\",\"x-aio-key: 27\"\r\n";
-    
-    //String http_str2 = "AT+HTTPPARA=\"CONTENT\",\"text/plain\"\r\n";
-    
-    String data_json = "{\"value\": 39, \"lat\": 23.1, \"lon\": \"-73.3\"}";
-    String data_plain = "\"value=42\"";
-
-    String data = data_json;
-    
-    String http_str5 = "AT+HTTPDATA=" + String(data.length()) + ",9900\r\n";
-
-    sendData("AT+HTTPINIT\r\n", 2000, DEBUG);
-
-    sendData(http_str, 1000, DEBUG);
-
-//    // GET
-//    sendData(http_str3, 1000, DEBUG);
-//    //sendData(http_str41, 1000, DEBUG);
-//    sendData(http_str4, 1000, DEBUG);
-//    sendData("AT+HTTPACTION=0\r\n", 5000, DEBUG);
-//    sendData("AT+HTTPHEAD\r\n", 5000, DEBUG);
-//    sendData("AT+HTTPREAD=0,200\r\n", 5000, DEBUG);
-
-        
-    //sendData(http_str2, 500, DEBUG);
-    //sendData(http_str3, 500, DEBUG);
-    //sendData(http_str35, 500, DEBUG);
-
-    sendData(http_str4, 2000, DEBUG);
-
-    //sendData(http_str41, 500, DEBUG);
-
-    sendData(http_str5, 500, DEBUG);
-    delay(100);
-    sendData(data, 2000, DEBUG);
-    
-    sendData("AT+HTTPACTION=1\r\n", 10000, DEBUG);
-
-    sendData("AT+HTTPHEAD\r\n", 5000, DEBUG);
-    sendData("AT+HTTPREAD=0,200\r\n", 5000, DEBUG);
-
-    
-    sendData("AT+HTTPTERM\r\n", 3000, DEBUG);
-
-    delay(5000);                                                      //Wait 120 seconds
+  delay(5000);
 }
 
 String sendData(String command, const int timeout, boolean debug)       //Function to send data over 4G

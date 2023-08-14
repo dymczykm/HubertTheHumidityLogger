c49e21633ee77de5ead655af409bc4d95a47800c
Marcin Dymczyk <marcin.dymczyk@sevensense.ch>
2023-08-14 09:19:14 +0200

    Restructure the dirs.

--- firmware/humidity_logger/influxdb.h
+++ firmware/humidity_logger/influxdb.h
@@ -1,7 +1,7 @@
 #include "sim7600.h"
 
-#define USER "***REMOVED***"
-#define PASSWORD "***REMOVED***"
-#define DB_URL "***REMOVED***"
+#define USER "xxx"
+#define PASSWORD "xxx"
+#define DB_URL "xxx"
 
 bool postToDatabase(String feed, const String& key, const float& temperature, const float& humidity, const float& pressure);
